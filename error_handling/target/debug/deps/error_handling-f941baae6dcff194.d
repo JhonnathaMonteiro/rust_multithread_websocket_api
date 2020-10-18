ed607c4c499771dc26f6073b26722bbee28133f4
Jhonnatha de Andrade Monteiro <jhonnatha@jhonnatha-mbp13.local>
2020-10-17 22:35:40 -0300

    second weekend

---
+++ error_handling/target/debug/deps/error_handling-f941baae6dcff194.d
@@ -0,0 +1,5 @@
+/Users/jhonnatha/Projects/rust/error_handling/target/debug/deps/error_handling-f941baae6dcff194.rmeta: src/main.rs
+
+/Users/jhonnatha/Projects/rust/error_handling/target/debug/deps/error_handling-f941baae6dcff194.d: src/main.rs
+
+src/main.rs:
---
+++ error_handling/target/rls/debug/deps/error_handling-7f72807fe6e0bc3a.d
@@ -0,0 +1,5 @@
+/Users/jhonnatha/Projects/rust/error_handling/target/rls/debug/deps/error_handling-7f72807fe6e0bc3a.rmeta: src/main.rs
+
+/Users/jhonnatha/Projects/rust/error_handling/target/rls/debug/deps/error_handling-7f72807fe6e0bc3a.d: src/main.rs
+
+src/main.rs:
